cb646c737a6b8f409b9cd180500f70252aaf3449
darksquare1 <nekit05@list.ru>
2023-11-04 10:27:33 +0300

    sorting tags by amount of pictures

--- njp/views.py
+++ njp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
+from django.db.models import Count
 from .models import *
 
 
@@ -10,5 +11,7 @@ class Index(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
+        popular_tags = Tag.objects.annotate(pic_count=Count('tags', distinct=True)).order_by('-pic_count')
+        context['popular_tags'] = popular_tags
         context['tags'] = Tag.objects.all()
         return context
